8f0633b6c645c2cc719868aa42add94da9486df6
Manish Rawat <manishrawat4u@gmail.com>
2020-02-08 12:42:51 -0500

    fixed cloudmailru

--- plugins/libs/cloudmailru.js
+++ plugins/libs/cloudmailru.js
@@ -12,18 +12,24 @@ class CloudMailRuResolver extends BaseUrlResolver {
 
     async resolveInner(_urlToResolve) {
         var links = [];
-        const response = await got(_urlToResolve);
-        var title = await x(response.body, 'title');
-        var regex01 = /"weblink_get"\s*:\s*\[.+?"url"\s*:\s*"([^"]+)/gs
-        var regex02 = /"tokens"\s*:\s*{\s*"download"\s*:\s*"([^"]+)/gs
-        var regex03 = /public\/(.*)/g
+        try {
+            const response = await got(_urlToResolve);
+            var title = await x(response.body, 'title');
+            var regex01 = /"weblink_get"\s*:\s*\[.+?"url"\s*:\s*"([^"]+)/gs
+            var regex02 = /"tokens"\s*:\s*{\s*"download"\s*:\s*"([^"]+)/gs
+            var regex03 = /public\/(.*)/g
 
-        var link1 = regex01.exec(response.body)[1];
-        var link2 = regex02.exec(response.body)[1];
-        var link3 = regex03.exec(_urlToResolve)[1];
+            var link1 = regex01.exec(response.body)[1];
+            var link2 = regex02.exec(response.body)[1];
+            var link3 = regex03.exec(_urlToResolve)[1];
 
-        var finalLink = `${link1}/${link3}?key=${link2}`;
-        links.push(BaseUrlResolver.prototype.createResult(title, finalLink, '', true));
+            var finalLink = `${link1}/${link3}?key=${link2}`;
+            var headers = await got.head(finalLink);
+            (headers.statusCode === 200) &&
+                links.push(BaseUrlResolver.prototype.createResult(title, finalLink, '', true));
+        } catch (error) {
+            console.log(`Error occurred while parsing cloudmailru link: ${_urlToResolve}`);
+        }
         return links;
     }
 }
