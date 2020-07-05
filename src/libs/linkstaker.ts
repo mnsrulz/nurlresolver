f9345229068e620dd7af332a429812a353d54cdf
Manish Rawat <manishrawat4u@gmail.com>
2020-07-05 00:55:14 -0400

    fixes for linkstaker and delete racatly

--- src/libs/linkstaker.ts
+++ src/libs/linkstaker.ts
@@ -13,24 +13,28 @@ export class LinkstakerResolver extends BaseUrlResolver {
             title: 'title',
             body: 'body@html'
         });
-        var regex_link = /"file": "(https:[^"]*)"/g;
-        var link = regex_link.exec(obj.body)![1];
+        const regex_link = /("file": "(https:[^"]*)")|(\("download"\)\.src="([^"]*))/g;
+        const regex_link_result = regex_link.exec(obj.body);
+        const link = regex_link_result && (regex_link_result[2] || regex_link_result[4]);
         if (link) {
-            var title = decodeURIComponent(obj.title).replace(/\+/g, ' ');
-            title = title.replace(' - Google Drive', '').trim();
-            var result = <ResolvedMediaItem>{
-                link: link,
-                title: title,
-                isPlayable: true
-            };
-            links.push(result);
-
             var gdriveUrl = new URL(link);
             if (gdriveUrl.hostname.toLowerCase().endsWith('googleusercontent.com')) {
+                console.log(`${_urlToResolve} : googleusercontent.com link found in the page, reconstructing the drive.google.com link.`);
                 const googleDocId = gdriveUrl.pathname.split('/').slice(-1)[0];
                 const constructedGoogleLInk = `https://drive.google.com/file/d/${googleDocId}/view`;
                 links.push({ link: constructedGoogleLInk, isPlayable: false } as ResolvedMediaItem);
+            } else {
+                var title = decodeURIComponent(obj.title).replace(/\+/g, ' ');
+                title = title.replace(' - Google Drive', '').trim();
+                var result = <ResolvedMediaItem>{
+                    link: link,
+                    title: title,
+                    isPlayable: true
+                };
+                links.push(result);
             }
+        } else {
+            console.log(`${_urlToResolve} : No valid link found in the page.`);
         }
         return links;
     }
