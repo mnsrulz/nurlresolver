8e1644f2fb4b8408946cb35f1c5ca0c4d006bac1
Manish Rawat <manishrawat4u@gmail.com>
2020-08-22 15:58:18 -0400

    made changes in the streamwire m3u8 generation

--- src/libs/streamwire.ts
+++ src/libs/streamwire.ts
@@ -19,6 +19,7 @@ export class StreamwireResolver extends BaseUrlResolver {
             const unpacked = unpacker.unpack(script);
             const sourceRx = /sources:\[{src:"([^"]*)"/;
             const link = sourceRx.exec(unpacked)![1];
+            const result = await this.gotInstance.head(link);   //doing this--as it's required to hit this so the media can be downloaded from any ip address            
             let title = link.endsWith('m3u8') ? obj.title + '.m3u8' : obj.title;
             links.push({ link, title, isPlayable: true } as ResolvedMediaItem);
         }
