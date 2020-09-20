1c3e4969f2e18e15be334c31d9ca32e61c84c69e
Manish Rawat <manishrawat4u@gmail.com>
2020-09-20 11:40:46 -0400

    fixes cric8cc

--- src/libs/cric8cc.ts
+++ src/libs/cric8cc.ts
@@ -3,22 +3,14 @@ import { BaseUrlResolver, ResolvedMediaItem } from "../BaseResolver";
 export class Cric8StreamResolver extends BaseUrlResolver {
     constructor() {
         super({
-            domains: [/https?:\/\/cric8\.cc\/stream/]
+            domains: [/https?:\/\/(www.)?cric8\.cc\/watch/]
         });
     }
 
     async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem[]> {
-        var links = [] as ResolvedMediaItem[];
         var response = await this.gotInstance(_urlToResolve);
-        var rx = /http:\/\/cric8\.cc\/watch[^"]*/g
-        var rxResult = response.body.match(rx);
-
-        if (rxResult) {
-            rxResult.forEach((el: any) => {
-                var result = { title: el, link: el } as ResolvedMediaItem;
-                links.push(result);
-            });
-        }
-        return links;
+        const result = await this.xInstance(response.body, 'iframe', [{ link: '@src' }]) as ResolvedMediaItem[];
+        console.log(result);
+        return result;
     }
 }
