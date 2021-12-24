1db4ce820302e04b2792af677290ff611e8b2443
mnsrulz <manishrawat4u@gmail.com>
2021-12-24 16:25:00 +0000

    fix uploadraja resolver

--- src/libs/uploadRaja.ts
+++ src/libs/uploadRaja.ts
@@ -1,10 +1,26 @@
-import { GenericFormBasedResolver } from "../BaseResolver";
+import { BaseUrlResolver, ResolvedMediaItem } from "../BaseResolver";
 
-export class UploadRajaResolver extends GenericFormBasedResolver {
+export class UploadRajaResolver extends BaseUrlResolver {
     constructor() {
         super({
             domains: [/https?:\/\/uploadraja/],
             speedRank: 90
-        }, 'a.download');
+        });
+    }
+
+    async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem | ResolvedMediaItem[]> {
+        const response = await this.gotInstance(_urlToResolve);
+        const response2 = await this.postHiddenForm(response.url, response.body, 0, false);
+        const link = response2.headers['location'];
+        if (link) {
+            const title = this.extractFileNameFromUrl(link);
+            const result = {
+                link,
+                title,
+                isPlayable: true
+            } as ResolvedMediaItem;
+            return [result];
+        }
+        return [];
     }
 }
