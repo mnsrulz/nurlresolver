c1271f7cf14da60e84e63291d6bbccc896427b47
Manish Rawat <manishrawat4u@gmail.com>
2022-02-12 21:05:42 -0500

    sl decyption added

--- src/libs/streamlare.ts
+++ src/libs/streamlare.ts
@@ -19,7 +19,7 @@ export class StreamlareResolver extends BaseUrlResolver {
         });
         const title = `${JSON.parse(fileEmbed).name}.mp4`;
         const jsontopost = { id: JSON.parse(fileEmbed).hashid };
-        
+
         const u1 = new URL('/api/video/get', _urlToResolve);
 
         const response2 = await this.gotInstance.post(u1.href, {
@@ -28,11 +28,23 @@ export class StreamlareResolver extends BaseUrlResolver {
         });
         const result = JSON.parse(response2) as streamlareResponseDto;
         if (result.status == 'success') {
-            const link = result.result['Original'].src;
+            const link = decryptString(result.result['Original'].src);
             return { link, title, isPlayable: true } as ResolvedMediaItem;
         }
         return [];
     }
+
+
+}
+
+const decryptString = (s: string) => {
+    const encodedString = Buffer.from(s, 'base64').toString('binary');  //atob
+    let result = '';
+    for (let i = 0; i < encodedString.length; i++) {
+        const tempResult = 51 ^ encodedString.charCodeAt(i);
+        result += String.fromCharCode(tempResult);
+    }
+    return result;
 }
 
 interface streamlareResponseDto {
