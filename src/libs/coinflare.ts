ab2f01934993390fb2c7a2755e942afe8ee12f6a
mnsrulz <manishrawat4u@gmail.com>
2022-07-19 02:24:05 +0000

    fixed coinflare

--- src/libs/coinflare.ts
+++ src/libs/coinflare.ts
@@ -11,7 +11,7 @@ export class coinflareResolver extends BaseUrlResolver {
         /*this may not work due to cf bot protection*/
         const docId = parseInt(new URL(_urlToResolve).pathname.split('/')[1]);
         const jsDocumentUrl = `https://coinflare.org/wp-json/wp/v2/posts/${docId}`;
-        const { body } = await this.gotInstance<Doc>(jsDocumentUrl);
+        const body = await this.gotInstance<Doc>(jsDocumentUrl).json<Doc>();
         let links = this.scrapeAllLinks(body.content.rendered, '.mb-container');
         return links;
     }
