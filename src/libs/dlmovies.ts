bbdff67db5a8943f7f6374e213dcb08b9a85bda0
mnsrulz <mnsrulz@gmail.com>
2022-11-14 23:50:47 -0500

    fixed links extralinks resolver

--- src/libs/dlmovies.ts
+++ src/libs/dlmovies.ts
@@ -18,15 +18,26 @@ export class dlmoviesResolver extends BaseUrlResolver {
         // console.log(_);
         // const response = await this.gotInstance(_, { resolveBodyOnly: true });
         // const rx = /download_video\('([^']*)','([^']*)','([^']*)'\)/g;
+
         // let m: RegExpExecArray | null;
-        // while (m = rx.exec(response)) {
-        //     //   console.log(m[1], m[2], m[3]);
+        // if (m = rx.exec(response)) {
+        //     //console.log(m[1]);
+        //     console.log(m[1], m[2], m[3]);
         //     const dlLink = `${new URL(_urlToResolve).origin}/dl?op=download_orig&id=${m[1]}&mode=${m[2]}&hash=${m[3]}`;
+        //     console.log(dlLink);
+        //     /*
+        //     https://dlmovies.link/dl?op=download_orig&id=4sbwkkevqu1m&mode=n&hash=47084043-11-76-1668484790-192a11013661a3b7504a634b2f457d32
+        //     https://dlmovies.link/dl?op=download_orig&id=4sbwkkevqu1m&mode=n&hash=47084043-73-188-1668484773-23deb9f9639040d6ea8ac02269a24bbe
+        //     */
+
+        //    /*
+        //    todo: solve google captcha
+        //    */
         //     const response2 = await this.gotInstance(dlLink, { resolveBodyOnly: true });
-        //     await this.postHiddenForm(dlLink, response2);
+        //     // await this.postHiddenForm(dlLink, response2);
         // }
 
-        // const result: any = [];        
+        // const result: any = [];
         // return result;
     }
 }
--- src/libs/linksExtralinksResolver.ts
+++ src/libs/linksExtralinksResolver.ts
@@ -8,13 +8,23 @@ export class LinksExtralinksResolver extends BaseUrlResolver {
     }
 
     async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem[]> {
+        console.log('coming3...')
         const response = await this.gotInstance(_urlToResolve);
         const result = this.scrapeAllLinks(response.body, '.entry-content');
+        console.log(result);
         result.forEach(x => {
             //find the link query param and extract result
             const link = new URL(x.link, response.url).searchParams.get('link');
-            link && (x.link = this.nodeatob(link));
+            if (link) {
+                x.link = atob(link)
+            } else {
+                const id = new URL(x.link, response.url).searchParams.get('id');
+                if (id) {
+                    const hostname = new URL(response.url).hostname.replace('links.','');
+                    x.link = `https://${hostname}/view/${atob(id)}`
+                }
+            }
         });
-        return result.filter(x=>x.link.startsWith('http'));
+        return result.filter(x => x.link.startsWith('http'));
     }
 }
