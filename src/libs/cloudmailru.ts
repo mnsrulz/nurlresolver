603217e36b9981f0608aff9a3ab28831144b704e
mnsrulz <mnsrulz@gmail.com>
2022-11-26 00:02:01 -0500

    fixed cloudmail resolver

--- src/libs/cloudmailru.ts
+++ src/libs/cloudmailru.ts
@@ -11,17 +11,15 @@ export class CloudMailRuResolver extends BaseUrlResolver {
 
     async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem[]> {
         const links = [] as ResolvedMediaItem[];
-
         const response = await this.gotInstance(_urlToResolve);
-        //var title = await this.xInstance(response.body, 'title');
-        const regex01 = /"weblink_get"\s*:\s*\[.+?"url"\s*:\s*"([^"]+)/gs
-        const regexWeblinkVideo = /"weblink_video"\s*:\s*\[.+?"url"\s*:\s*"([^"]+)/gs
-        const regex03 = /public\/(.*)/g
-        const link1 = regex01.exec(response.body)?.[1];
-        const link3 = regex03.exec(_urlToResolve)?.[1];
-        const webLinkVideo = regexWeblinkVideo.exec(response.body)?.[1];
-        if (link1 && link3 && webLinkVideo) {
-            const tempLink = `${link1}/${link3}`;
+        const regex01 = /"weblink_get":({[^}]*})/gs
+        const regex02 = /public\/(.*)/g
+        const jsonObject = regex01.exec(response.body)?.[1] || '';
+        const {url} = JSON.parse(jsonObject);
+        const videoId = regex02.exec(_urlToResolve)?.[1];
+
+        if (url && videoId) {
+            const tempLink = `${url}/${videoId}`;
             const headers = await this.gotInstance.head(tempLink);
             if (headers.statusCode === 200) {
                 const finalLink = headers.url;
