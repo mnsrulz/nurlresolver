c1f4ef5503e66e8392bcdd0f00fea661524dbbd2
mnsrulz <manishrawat4u@gmail.com>
2023-03-15 01:18:00 +0000

    fix uploadhub resolver

--- src/libs/uploadhubResolver.ts
+++ src/libs/uploadhubResolver.ts
@@ -3,7 +3,7 @@ import { GenericFormBasedResolver } from "../BaseResolver";
 export class UploadhubResolver extends GenericFormBasedResolver {
     constructor() {
         super({
-            domains: [/https?:\/\/(uploadhub.ws)/],
+            domains: [/https?:\/\/(uploadhub.ws|uploadhub.pw)/],
             useCookies: true,
             speedRank: 60
         }, '#direct_link a');
