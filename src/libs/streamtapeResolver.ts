820d76c5a4e03f0ef2bb1bf16ef8a34c0df926c3
mnsrulz <manishrawat4u@gmail.com>
2023-03-16 03:31:08 +0000

    fix streamtape

--- src/libs/streamtapeResolver.ts
+++ src/libs/streamtapeResolver.ts
@@ -15,10 +15,16 @@ export class streamtapeResolver extends BaseUrlResolver {
         initialUrl.pathname = initialUrl.pathname.replace('/e/', '/v/'); //replace the embed video to download link
         const response2 = await this.gotInstance(initialUrl.href);
         const title = this.scrapeInnerText(response2.body, '.video-title h2');
-        const roboLink = this.scrapeInnerText(response2.body, '#norobotlink');
-        if (roboLink) {
-            const link = `https:/${roboLink}&dl=1`;
-            console.log(link);
+        const roboLinkText = this.scrapeInnerText(response2.body, '#norobotlink');
+        const roboLink = `https:/${roboLinkText}`;
+        const parsedUrl = new URL(roboLink);
+        const searchParams = parsedUrl.searchParams;
+        const rx = /document\.getElementById\('ideoooolink'\)\.innerHTML.*token=([^']*)/g
+        const tokenRxResult = rx.exec(response2.body);
+        const token = tokenRxResult?.[1];
+        if (token) {
+            searchParams.set('token', token);
+            const link = `${parsedUrl.origin}${parsedUrl.pathname}?${searchParams.toString()}&dl=1`;
             const result = { link, title, isPlayable: true } as ResolvedMediaItem;
             return [result];
         }
