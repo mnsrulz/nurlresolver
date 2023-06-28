2b07c524643fd00780d8a64f7474399a686a70f8
mnsrulz <manishrawat4u@gmail.com>
2023-06-28 04:51:20 +0000

    fix gofile

--- src/libs/gofile.ts
+++ src/libs/gofile.ts
@@ -1,6 +1,18 @@
 import { BaseUrlResolver, ResolvedMediaItem } from "../BaseResolver.js";
+let globaltoken = '';
 
 export class GoFileResolver extends BaseUrlResolver {
+    private async fetchGlobalToken() {
+        //instead of creating account every time let's cache it at app level
+        if (!globaltoken) {
+            const apitoken = await this.gotInstance('https://api.gofile.io/createAccount')
+                .json<{ data: { token: string }, status: string }>();
+            const { token } = apitoken.data;
+            globaltoken = token;
+        }
+        return globaltoken;
+
+    }
     constructor() {
         super({
             domains: [/https?:\/\/gofile\.io/],
@@ -10,20 +22,22 @@ export class GoFileResolver extends BaseUrlResolver {
 
     async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem[]> {
         const initialResponse = await this.gotInstance(_urlToResolve);
-        const rxp = /gofile\.io\/d\/(\w*)/;
-        const gofileId = rxp.exec(initialResponse.url)?.[1]; //extract go fileid
-        const token = 'tqY41X5Mei9MB0L0D25qyNVSbc7ZRjED';
-        const apiUrl = `https://api.gofile.io/getContent?contentId=${gofileId}&token=${token}&websiteToken=12345`;
+        const gofileId = new URL(initialResponse.url).pathname.split('/').pop(); //extract go fileid
+        const token = await this.fetchGlobalToken();
+        const apiUrl = `https://api.gofile.io/getContent?contentId=${gofileId}&token=${token}&websiteToken=7fd94ds12fds4`;
         const { data } = await this.gotInstance<ResponsePayload>(apiUrl, {
             resolveBodyOnly: true,
             responseType: 'json'
         });
 
         const firstValidFile = data.contents[Object.keys(data.contents)[0]];
-
-        return [{
-            isPlayable: true, title: firstValidFile.name, link: firstValidFile.link
-        } as ResolvedMediaItem];
+        const rs = {
+            isPlayable: true,
+            title: firstValidFile.name,
+            link: firstValidFile.link
+        } as ResolvedMediaItem;
+        rs.headers = { "Cookie": `accountToken=${token}` }
+        return [rs];
     }
 }
 
@@ -36,3 +50,52 @@ interface ResponsePayload {
         }>
     }
 }
+
+
+
+
+
+// fetch("https://store1.gofile.io/download/207ed333-aa6c-4d2d-9875-351dc42fdb54/Nayika.Devi.The.Warrior.Queen.2022.480p.WEB.HDRip.HINDI.HQ.Dub.2.0.x264.HDHub4u.mkv", {
+//   "headers": {
+//     "accept": "*/*",
+//     "accept-language": "en-US,en;q=0.8",
+//     "cache-control": "no-cache",
+//     "pragma": "no-cache",
+//     "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Brave\";v=\"114\"",
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-ch-ua-platform": "\"Windows\"",
+//     "sec-fetch-dest": "empty",
+//     "sec-fetch-mode": "cors",
+//     "sec-fetch-site": "same-site",
+//     "sec-gpc": "1"
+//   },
+//   "referrerPolicy": "no-referrer",
+//   "body": null,
+//   "method": "GET",
+//   "mode": "cors",
+//   "credentials": "omit"
+// });
+
+// fetch("https://store1.gofile.io/download/207ed333-aa6c-4d2d-9875-351dc42fdb54/Nayika.Devi.The.Warrior.Queen.2022.480p.WEB.HDRip.HINDI.HQ.Dub.2.0.x264.HDHub4u.mkv", {
+//   "headers": {
+//     "accept": "*/*",
+//     "accept-language": "en-US,en;q=0.8",
+//     "cache-control": "no-cache",
+//     "pragma": "no-cache",
+//     "range": "bytes=0-",
+//     "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Brave\";v=\"114\"",
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-ch-ua-platform": "\"Windows\"",
+//     "sec-fetch-dest": "video",
+//     "sec-fetch-mode": "no-cors",
+//     "sec-fetch-site": "same-site",
+//     "sec-gpc": "1"
+//   },
+//   "referrerPolicy": "no-referrer",
+//   "body": null,
+//   "method": "GET",
+//   "mode": "cors",
+//   "credentials": "include"
+// });
+
+// */
