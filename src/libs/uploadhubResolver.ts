d8d0e40972458335211c038bfeed799409659f16
mnsrulz <manishrawat4u@gmail.com>
2023-08-24 03:33:26 +0000

    fix uploadhub

--- src/libs/uploadhubResolver.ts
+++ src/libs/uploadhubResolver.ts
@@ -3,7 +3,7 @@ import { GenericFormBasedResolver } from "../BaseResolver.js";
 export class UploadhubResolver extends GenericFormBasedResolver {
     constructor() {
         super({
-            domains: [/https?:\/\/(uploadhub.ws|uploadhub.pw)/],
+            domains: [/https?:\/\/uploadhub/],
             useCookies: true,
             speedRank: 60
         }, '#direct_link a');
