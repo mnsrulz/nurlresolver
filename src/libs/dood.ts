bf7e030d3ccb455d04c390d7752bd680cabdb426
mnsrulz <mnsrulz@gmail.com>
2023-09-17 00:16:00 -0400

    fix doodstream

--- src/libs/dood.ts
+++ src/libs/dood.ts
@@ -1,5 +1,4 @@
 import { BaseUrlResolver, ResolvedMediaItem } from "../BaseResolver.js";
-
 export class DoodResolver extends BaseUrlResolver {
     constructor() {
         super({
@@ -9,21 +8,18 @@ export class DoodResolver extends BaseUrlResolver {
         });
     }
 
-    async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem[]> {
-        const identifier = new URL(_urlToResolve).pathname.split('/').pop();
-        const result = await this.gotInstance(`https://dood.so/d/${identifier}`); //dood.so works but not watch
-        await this.wait(1000);  //this could be trickier
-        const allLinks = this.scrapeAllLinks(result.body, '.download-content', result.url);
-        const response2 = await this.gotInstance(allLinks[0].link);
-        const rx = /window\.open\('(http[^']*)'/
-        const matches = rx.exec(response2.body);
-        const link = matches?.[1];
-
+    async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem | ResolvedMediaItem[]> {
+        let finalRequestUrl = '';
+        const r = await fetch(_urlToResolve).then(r => { finalRequestUrl = r.url; return r.text(); });
+        await this.wait(5000);
+        const b2Link = new URL(this.scrapeLinkHref(r, '.download-content a'), finalRequestUrl).href;
+        const r2 = await fetch(b2Link).then(r => r.text());
+        const link = /window\.open\('(http[^']*)'/.exec(r2)?.[1];
         if (link) {
             const title = this.extractFileNameFromUrl(link);
             const result = { link, title: title, isPlayable: true } as ResolvedMediaItem;
-            result.headers = { 'Referer': response2.url };
-            return [result];
+            result.headers = { 'Referer': _urlToResolve };
+            return result;
         } else {
             return [];
         }
