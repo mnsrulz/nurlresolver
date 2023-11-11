162252dbc4a56192ac8b98b24813ad9bfaac3ef7
mnsrulz <manishrawat4u@gmail.com>
2023-11-11 21:53:58 +0000

    fix panadafiles

--- src/libs/pandafiles.ts
+++ src/libs/pandafiles.ts
@@ -1,30 +1,64 @@
 import { BaseUrlResolver, ResolvedMediaItem } from "../BaseResolver.js";
+//this is the sample captcha response
+const shtml = `<div class="captcha"><tr><td align="right"><div style="width:80px;height:26px;font:bold 13px Arial;background:#ccc;text-align:left;direction:ltr;">
+    <span style="position:absolute;padding-left:43px;padding-top:6px;">3</span>
+    <span style="position:absolute;padding-left:27px;padding-top:7px;">5</span>
+    <span style="position:absolute;padding-left:60px;padding-top:3px;">9</span>
+    <span style="position:absolute;padding-left:7px;padding-top:4px;">2</span>
+    </div></td></tr></div>`;
 
 export class PandafilesResolver extends BaseUrlResolver {
     constructor() {
         super({
             domains: [/https?:\/\/(pandafiles)/],
             useCookies: true,
-            speedRank: 50
+            speedRank: 85
         });
     }
 
-    async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem | ResolvedMediaItem[]> {
-        return [];
-        //it requires captcha!!!
-        // const response = await this.gotInstance(_urlToResolve);
-        // const form = this.getHiddenForm(response.body);
-        // if (!form) return [];
+    private parseCaptchaCode(html: string): string {
+        const result: { code: [{ codeValue: string, codePosition: number }] } = this.scrapeHtml(html, {
+            code: {
+                listItem: '.captcha span',
+                data: {
+                    codeValue: {},
+                    codePosition: {
+                        attr: 'style',
+                        convert: (x) => {
+                            const rxResult = /padding-left:(\d*)px/.exec(x);
+                            return rxResult && parseInt(rxResult[1]);
+                        }
+                    }
+                }
+            }
+        });
 
-        // form['op'] = 'download2';
-        // const resp = await this.gotInstance.post(response.url, { form: form });
-        // const link = this.scrapeLinkHref(resp.body, '#direct_link a');
-        // const title = this.extractFileNameFromUrl(link);
-        // const result = {
-        //     link,
-        //     title,
-        //     isPlayable: true
-        // } as ResolvedMediaItem;
-        // return [result];
+        return result.code
+            .sort((a, b) => a.codePosition - b.codePosition)
+            .map(x => x.codeValue)
+            .join('');
+    }
+
+
+    async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem | ResolvedMediaItem[]> {
+        const response = await this.gotInstance(_urlToResolve);
+        const form = this.getHiddenForm(response.body);
+        if (!form) return [];
+        delete form['method_premium'];
+        const resp1 = await this.gotInstance.post(response.url, { form: form });
+        const captchaCode = this.parseCaptchaCode(resp1.body);
+        const form2 = this.getHiddenForm(resp1.body);
+        if (!form2) return []
+        form2['referer'] = response.url;
+        form2['code'] = captchaCode;
+        const resp2 = await this.gotInstance.post(resp1.url, { form: form2 });
+        const link = this.scrapeLinkHref(resp2.body, '#direct_link a');
+        const title = this.extractFileNameFromUrl(link);
+        const result = {
+            link,
+            title,
+            isPlayable: true
+        } as ResolvedMediaItem;
+        return [result];
     }
 }
