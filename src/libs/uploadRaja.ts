1e5dce8808133f1c97ef70a9a90199dd13482eae
mnsrulz <manishrawat4u@gmail.com>
2023-11-21 05:11:54 +0000

    fix uploadraja

--- src/libs/uploadRaja.ts
+++ src/libs/uploadRaja.ts
@@ -4,22 +4,46 @@ export class UploadRajaResolver extends BaseUrlResolver {
     constructor() {
         super({
             domains: [/https?:\/\/uploadraja/],
-            speedRank: 90
+            speedRank: 85,
+            useCookies: true
         });
     }
 
+    private parseCaptchaCode(html: string): string {
+        const result: { code: [{ codeValue: string, codePosition: number }] } = this.scrapeHtml(html, {
+            code: {
+                listItem: '#commonId td span',
+                data: {
+                    codeValue: {},
+                    codePosition: {
+                        attr: 'style',
+                        convert: (x) => {
+                            const rxResult = /padding-left:(\d*)px/.exec(x);
+                            return rxResult && parseInt(rxResult[1]);
+                        }
+                    }
+                }
+            }
+        });
+
+        return result.code
+            .sort((a, b) => a.codePosition - b.codePosition)
+            .map(x => x.codeValue)
+            .join('');
+    }
+
     async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem | ResolvedMediaItem[]> {
-        const response = await this.gotInstance(_urlToResolve);
-        const response2 = await this.postHiddenForm(response.url, response.body, 0, false);
-        const link = this.scrapeLinkHref(response2.body, 'a.download');
+        const resp1 = await this.gotInstance(_urlToResolve);
+        const form = this.getHiddenForm(resp1.body);
+        if (!form) return [];
+        const captchaCode = this.parseCaptchaCode(resp1.body);
+        form['code'] = captchaCode;
+        await this.wait(5000);
+        const resp2 = await this.gotInstance.post(resp1.url, { form: form });
+        const link = this.scrapeLinkHref(resp2.body, '#direct_link a');
         if (link) {
             const title = this.extractFileNameFromUrl(link);
-            const result = {
-                link,
-                title,
-                isPlayable: true
-            } as ResolvedMediaItem;
-            return [result];
+            return { link, title, isPlayable: true } as ResolvedMediaItem;
         }
         return [];
     }
