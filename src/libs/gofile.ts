8a11d6774b95a8283d2328ca9674d44215be4854
mnsrulz <manishrawat4u@gmail.com>
2024-03-09 23:19:02 +0000

    fix gofile

--- src/libs/gofile.ts
+++ src/libs/gofile.ts
@@ -24,7 +24,7 @@ export class GoFileResolver extends BaseUrlResolver {
         const initialResponse = await this.gotInstance(_urlToResolve);
         const gofileId = new URL(initialResponse.url).pathname.split('/').pop(); //extract go fileid
         const token = await this.fetchGlobalToken();
-        const apiUrl = `https://api.gofile.io/getContent?contentId=${gofileId}&token=${token}&websiteToken=7fd94ds12fds4`;
+        const apiUrl = `https://api.gofile.io/getContent?contentId=${gofileId}&token=${token}&wt=4fd6sg89d7s6`;
         const { data } = await this.gotInstance<ResponsePayload>(apiUrl, {
             resolveBodyOnly: true,
             responseType: 'json'
@@ -50,52 +50,3 @@ interface ResponsePayload {
         }>
     }
 }
-
-
-
-
-
-// fetch("https://store1.gofile.io/download/207ed333-aa6c-4d2d-9875-351dc42fdb54/Nayika.Devi.The.Warrior.Queen.2022.480p.WEB.HDRip.HINDI.HQ.Dub.2.0.x264.HDHub4u.mkv", {
-//   "headers": {
-//     "accept": "*/*",
-//     "accept-language": "en-US,en;q=0.8",
-//     "cache-control": "no-cache",
-//     "pragma": "no-cache",
-//     "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Brave\";v=\"114\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-ch-ua-platform": "\"Windows\"",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "same-site",
-//     "sec-gpc": "1"
-//   },
-//   "referrerPolicy": "no-referrer",
-//   "body": null,
-//   "method": "GET",
-//   "mode": "cors",
-//   "credentials": "omit"
-// });
-
-// fetch("https://store1.gofile.io/download/207ed333-aa6c-4d2d-9875-351dc42fdb54/Nayika.Devi.The.Warrior.Queen.2022.480p.WEB.HDRip.HINDI.HQ.Dub.2.0.x264.HDHub4u.mkv", {
-//   "headers": {
-//     "accept": "*/*",
-//     "accept-language": "en-US,en;q=0.8",
-//     "cache-control": "no-cache",
-//     "pragma": "no-cache",
-//     "range": "bytes=0-",
-//     "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Brave\";v=\"114\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-ch-ua-platform": "\"Windows\"",
-//     "sec-fetch-dest": "video",
-//     "sec-fetch-mode": "no-cors",
-//     "sec-fetch-site": "same-site",
-//     "sec-gpc": "1"
-//   },
-//   "referrerPolicy": "no-referrer",
-//   "body": null,
-//   "method": "GET",
-//   "mode": "cors",
-//   "credentials": "include"
-// });
-
-// */
