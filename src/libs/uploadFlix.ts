2a96612ad3663c818c2d4f2e762a5c4c329618e5
mnsrulz <manishrawat4u@gmail.com>
2024-09-01 17:57:01 +0000

    uploadflix fix

--- src/libs/uploadFlix.ts
+++ src/libs/uploadFlix.ts
@@ -1,10 +1,24 @@
-import { GenericFormBasedResolver } from "../BaseResolver.js";
+import { BaseUrlResolver, ResolvedMediaItem } from "../BaseResolver.js";
 
-export class UploadFlixResolver extends GenericFormBasedResolver {
+export class UploadFlixResolver extends BaseUrlResolver {
     constructor() {
         super({
             domains: [/https?:\/\/(dl\.)?uploadflix/],
-            speedRank: 75
-        }, 'a.downloadbtn');
+            speedRank: 85
+        });
+    }
+
+    async resolveInner(_urlToResolve: string): Promise<ResolvedMediaItem[] | ResolvedMediaItem> {
+        const resp = await this.gotInstance(_urlToResolve);
+        const link = this.scrapeLinkHref(resp.body, '.download a');
+        if(link){
+            const title = this.extractFileNameFromUrl(link)
+            return  {
+                link,
+                title,
+                isPlayable: true
+            } as ResolvedMediaItem;   
+        }
+        return [];
     }
 }
